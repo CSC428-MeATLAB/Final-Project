4a4f383e9c40b91b3ea8f8b2a6bff60e389848b7
Ryan Zesch <ryan.zesch@gmail.com>
2020-03-07 21:13:10 -0800

    Now chack for hammer-ons seperately

--- convert_video.py
+++ convert_video.py
@@ -10,14 +10,14 @@ AREA_MAX_THRESHOLD = 3000
 FRAME_SKIP_COUNT = 2
 
 # Control the framerate
-DELAY = 1/6000
+DELAY = 1/600
 
 # Title of the window
 WINDOW_TITLE = 'Video'
 
 # Percentage of screen to use for note detection
 CROP_WIDTH = .5
-CROP_HEIGHT = .5
+CROP_HEIGHT = .35
 
 # Define Window settings
 cv2.namedWindow(WINDOW_TITLE, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
@@ -26,9 +26,13 @@ cv2.resizeWindow(WINDOW_TITLE, 1500,1000)
 # Create a VideoCapture object and read from input file
 cap = cv2.VideoCapture('bot-no-effects-720p.mp4')
 
-# Load in a blue note template
+# Load in a template
 template = cv2.imread("notecenter.png", cv2.IMREAD_GRAYSCALE)
-temp_w, temp_h = template.shape[::-1]
+reg_temp_w, reg_temp_h = template.shape[::-1]
+
+# Load in a hammer note template
+ham_template = cv2.imread("hammercenter.png", cv2.IMREAD_GRAYSCALE)
+ham_temp_w, ham_temp_h = ham_template.shape[::-1]
 
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
@@ -64,26 +68,39 @@ def box_image(img: np.array):
 
     # Compute image bounds for template matching
     top_bound = int(img.shape[0] * (1-CROP_HEIGHT))
-    bottom_bound = int(img.shape[0] * 14.0/15)
+    bottom_bound = int(img.shape[0] * 13.0/15)
     left_bound = int(img.shape[1]/2 - img.shape[1] * (1-CROP_WIDTH)/2)
     right_bound = int(img.shape[1]/2 + img.shape[1] * (1-CROP_WIDTH)/2)
 
     # Crop image
     crop_img = img[top_bound:bottom_bound , left_bound:right_bound]
     gray_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
 
-    # Filter down to points were sure about
+    # Detect regular and hammer on notes
+    result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
+    result2 =cv2.matchTemplate(gray_img, ham_template, cv2.TM_CCOEFF_NORMED)
+
+    # Filter down to notes we're sure about
     loc = np.where(result >= 0.8)
     loc = nonMaxSupp(loc, result)
+    loc2 = np.where(result2 >= 0.7)
+    loc2 = nonMaxSupp(loc2, result2)
 
-    # Add all the points to an array
+    # Add regular notes to the array
     for pt in zip(*loc[::-1]):
-        subimg = gray_img[pt[1] + int(.33 * temp_h) : pt[1] + int(.66 * temp_h) , pt[0] + int(.33 * temp_w) : pt[0] + int(.66 * temp_w)]
+        subimg = gray_img[pt[1] + int(.33 * reg_temp_h) : pt[1] + int(.66 * reg_temp_h) , pt[0] + int(.33 * reg_temp_w) : pt[0] + int(.66 * reg_temp_w)]
         center_shade = np.average(np.average(subimg, axis=0), axis=0)
         # Make sure the center of the note is white (so we dont detect the bottom bar) 
         if (center_shade > 190):
-            rect_list.append((pt[0] + left_bound, pt[1] + top_bound, temp_w, temp_h))
+            rect_list.append((pt[0] + left_bound, pt[1] + top_bound, reg_temp_w, reg_temp_h))
+
+    # Add hammer on notes to the array
+    for pt in zip(*loc2[::-1]):
+        subimg = gray_img[pt[1] + int(.33 * ham_temp_h) : pt[1] + int(.66 * ham_temp_h) , pt[0] + int(.33 * ham_temp_w) : pt[0] + int(.66 * ham_temp_w)]
+        center_shade = np.average(np.average(subimg, axis=0), axis=0)
+        # Make sure the center of the note is white (so we dont detect the bottom bar) 
+        if (center_shade > 190):
+            rect_list.append((pt[0] + left_bound, pt[1] + top_bound, ham_temp_w, ham_temp_h))
 
     return rect_list
 
