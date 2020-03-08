4f895e763702f5687ffb50838b9f6ae3660d687d
JaredPC <jaredpc12@gmail.com>
2020-03-07 19:24:26 -0800

    Screen Capture code and button press function

--- convert_live_video.py
+++ convert_live_video.py
@@ -1,9 +1,15 @@
 import cv2
 import numpy as np
 import time
+import mss
+from pynput.keyboard import Key, Controller
+
 
 # Minimim area threshold that is boxed
 AREA_THRESHHOLD = 1000
+
+# If using a camera this is set as True
+LIVE_CAMERA = False
 
 # Number of frames to skip to calculate the box
 FRAME_SKIP_COUNT = 2
@@ -11,16 +17,27 @@ FRAME_SKIP_COUNT = 2
 # Title of the window
 WINDOW_TITLE = 'Video'
 
+# The Window Number to capture
+WINDOW_NUM = 1
+
+# Create the controller object
+keyboard = Controller()
+
+# Keyboard to button matchings (Default)
+GREEN = 'a'
+RED = 's'
+YELLOW = 'j'
+BLUE = 'k'
+ORANGE = 'l'
+
 # Define Window settings
 cv2.namedWindow(WINDOW_TITLE, cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
 cv2.resizeWindow(WINDOW_TITLE, 1500,1000)
 
-# Create a VideoCapture object and read from input file
-cap = cv2.VideoCapture(0)
+# current frame counter
+count = 0
+rect_list = []
 
-# Check if camera opened successfully
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
 
 # Function that takes in a image and draws boxes around suspected plants
 def box_image(img: np.array):
@@ -54,40 +71,84 @@ def box_image(img: np.array):
             # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 15)
     return rect_list
 
-# current frame counter
-count = 0
-rect_list = []
 
-# Read until video is completed
-while(cap.isOpened()):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if ret == True:
-        # t0 = time.time()
-        count = count + 1
-        if ((count % FRAME_SKIP_COUNT) == 0):
-            rect_list = box_image(frame)
-        # t1 = time.time()
-
-        for rects in rect_list:
-            x, y, w, h = rects
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 15)
-
-        # Display the resulting frame
-        cv2.imshow(WINDOW_TITLE,frame)
-
-        # Press Q on keyboard to  exit
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-        # print(f'Frame {count} Calc Time: {t1-t0}')
-
-    # Break the loop
+def press_key(key, release = False):
+    if(release):
+        keyboard.release(key)
     else:
-        break
+        keyboard.press(key)
 
-# When everything done, release the video capture object
-cap.release()
+
+
+# Use video from Camera
+if(LIVE_CAMERA):
+    # Create a VideoCapture object and read from input file
+    cap = cv2.VideoCapture(0)
+
+    # Check if camera opened successfully
+    if (cap.isOpened()== False): 
+        print("Error opening video stream or file")
+    
+    # Read until video is completed
+    while(cap.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == True:
+            # #DEBUG# t0 = time.time()
+            count = count + 1
+            if ((count % FRAME_SKIP_COUNT) == 0):
+                rect_list = box_image(frame)
+            # #DEBUG# t1 = time.time()
+            # #DEBUG# print(f'Frame {count} Calc Time: {t1-t0}')
+            for rects in rect_list:
+                x, y, w, h = rects
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 15)
+
+            # Display the resulting frame
+            cv2.imshow(WINDOW_TITLE,frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+        # Break the loop
+        else:
+            break
+    
+    # When everything done, release the video capture object
+    cap.release()
+
+
+# Use Video from Screen Capture
+else:
+    # Library to do screen capture
+    with mss.mss() as sct:
+        # Which monitor to capture
+        mon = sct.monitors[WINDOW_NUM]
+        while True:
+            # Capture frame
+            frame = np.array(sct.grab(mon))
+
+            # #DEBUG# t0 = time.time()
+            count = count + 1
+            if ((count % FRAME_SKIP_COUNT) == 0):
+                rect_list = box_image(frame)
+            # #DEBUG# t1 = time.time()
+            # #DEBUG# print(f'Frame {count} Calc Time: {t1-t0}')
+
+            for rects in rect_list:
+                x, y, w, h = rects
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 15)
+
+            # Display the resulting frame
+            cv2.imshow(WINDOW_TITLE,frame)
+
+            # Press Q on keyboard to  exit
+            if cv2.waitKey(1) == ord('q'):
+                break
 
 # Closes all the frames
 cv2.destroyAllWindows()
+
+
+
